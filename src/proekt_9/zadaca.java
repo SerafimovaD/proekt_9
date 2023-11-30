8f3cbc4e054365a73672d37304e16a82a7453871
SerafimovaD <daci.serafimova@gmail.com>
2023-11-30 09:04:10 +0100

    Add first java program

---
+++ src/proekt_9/zadaca.java
@@ -0,0 +1,14 @@
+package proekt_9;
+
+public class zadaca {
+	void bark() {
+		System.out.println("Woof-Woof");
+	}
+	void zadaca() {
+		int a=10;
+		int b=5;
+		int c=a+b;
+		System.out.println(c);
+	}
+
+}
---
+++ src/proekt_9/zadaca1.java
@@ -0,0 +1,10 @@
+package proekt_9;
+
+public class zadaca1 {
+
+	public static void main(String[] args) {
+		zadaca dog=new zadaca();
+		dog.bark();
+	}
+
+}
